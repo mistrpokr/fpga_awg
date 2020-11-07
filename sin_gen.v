b5e71c00fb32cbd6e0a5dd84b2d7836117dffd4b
pokr <mistrpokr@outlook.com>
2020-11-08 00:47:08 +0800

    Rename variables;Deprecate addr_step:Could be a width problem, need further investigating(or it just works)

--- sin_gen.v
+++ sin_gen.v
@@ -10,27 +10,29 @@ module sin_gen (en,
     input [11:0] state_freq;
     input [2:0] state_amp;
     input [7:0] state_phase;
-    output [13:0] DAC_in; 
+    output [13:0] DAC_in;
     
     reg [18:0] addr;
-    reg [13:0] sin_amp;
-    wire [13:0] sin_w;
+    reg [13:0] data_amp;
+    wire [13:0] data;
     wire [15:0] addr_step;
     
-	initial addr <= 1'd0; 
-
-    assign DAC_in[13:0]    = en?sin_w:14'b0;
+    initial addr <= 19'd0;
+    
+    assign DAC_in[13:0]    = en?data:14'b0;
     assign addr_step[11:0] = state_freq[11:0];
     
     always @(posedge clk) begin
-        addr       <= addr + state_freq;
-        // sin_amp <= (sin_w - 14'd8192)/(state_amp)*(sin_w[13]?-1'd1:-1'd1) + 14'd8192;
-        // sin_amp <= (sin_w - 14'd8192)/(state_amp) + 14'd8192;
-        sin_amp    <= (sin_w[13]?(sin_w-14'd8192)/state_amp+14'd8192:14'd8192-(14'd8192-sin_w)/state_amp);
+        addr        <= addr + state_freq; 
+        // data_amp <= (data - 14'd8192)/(state_amp)*(data[13]?-1'd1:-1'd1) + 14'd8192;
+        // data_amp <= (data - 14'd8192)/(state_amp) + 14'd8192;
+        data_amp <= (data[13]?
+        (data-14'd8192)/state_amp+14'd8192
+        :14'd8192-(14'd8192-data)/state_amp);
     end
     
-    sqr_table sin_tab_inst(
+    sin_table sin_tab_inst(
     .address(addr[18:9]),
-    .data(sin_w)
+    .data(data)
     );
 endmodule
