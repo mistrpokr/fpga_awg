10856569c8222fd5c23a8eea83995ae59814a3d7
pokr <mistrpokr@outlook.com>
2020-11-08 22:15:54 +0800

    Extend channel B for wave_gen submodules

--- sin_gen.v
+++ sin_gen.v
@@ -3,30 +3,35 @@ module sin_gen (en,
                 freq,
                 amp,
                 phase,
-                DAC_in);
-    // input [15:0] addr;
+                DAC_out_A,
+                DAC_out_B);
+
     input en;
     input clk;
     input [11:0] freq;
     input [2:0] amp;
     input [7:0] phase;
-    output [13:0] DAC_in;
+    output [13:0] DAC_out_A;
+    output [13:0] DAC_out_B;
     
     reg [18:0] addr;
     reg [13:0] data_amp;
+    reg [13:0] data_phase_amp;
     wire [13:0] data;
     wire [15:0] addr_step;
     
     initial addr <= 19'd0;
     
-    assign DAC_in[13:0]    = en?data:14'b0;
+    assign DAC_out_A[13:0] = en?data_amp:14'b0;
+    assign DAC_out_B[13:0] = en?data_phase_amp:14'b0;
     assign addr_step[11:0] = freq[11:0]; // TODO unused addr_step
     
     always @(posedge clk) begin
-        addr        <= addr + freq;
-        // data_amp <= (data - 14'd8192)/(amp)*(data[13]?-1'd1:-1'd1) + 14'd8192;
-        // data_amp <= (data - 14'd8192)/(amp) + 14'd8192;
+        addr <= addr + freq;
         data_amp <= (data[13]?
+        (data-14'd8192)/amp+14'd8192
+        :14'd8192-(14'd8192-data)/amp);
+        data_phase_amp <= (data[13]?
         (data-14'd8192)/amp+14'd8192
         :14'd8192-(14'd8192-data)/amp);
     end
@@ -35,4 +40,9 @@ module sin_gen (en,
     .address(addr[18:9]),
     .data(data)
     );
+    
+    sin_table sin_phase(
+    .address(addr[18:9] + phase),
+    .data(data_phase)
+    )
 endmodule
--- sqr_gen.v
+++ sqr_gen.v
@@ -3,27 +3,34 @@ module sqr_gen (en,
                 freq,
                 amp,
                 phase,
-                DAC_in);
+                DAC_out_A,
+                DAC_out_B);
     input en;
     input clk;
     input [11:0] freq;
     input [2:0] amp;
     input [7:0] phase;
-    output [13:0] DAC_in;
+    output [13:0] DAC_out_A;
+    output [13:0] DAC_out_B;
     
     reg [18:0] addr;
     reg [13:0] data_amp;
+    reg [13:0] data_phase_amp;
     wire [13:0] data;
     wire [15:0] addr_step;
     
     initial addr <= 19'd0;
     
-    assign DAC_in[13:0]    = en?data:14'b0;
-    assign addr_step[11:0] = freq[11:0];
+    assign DAC_out_A[13:0] = en?data_amp:14'b0;
+    assign DAC_out_B[13:0] = en?data_phase_amp:14'b0;
+    assign addr_step[11:0] = freq[11:0]; // TODO unused addr_step
     
     always @(posedge clk) begin
         addr <= addr + freq;
         data_amp <= (data[13]?
+        (data-14'd8192)/amp+14'd8192
+        :14'd8192-(14'd8192-data)/amp);
+        data_phase_amp <= (data[13]?
         (data-14'd8192)/amp+14'd8192
         :14'd8192-(14'd8192-data)/amp);
     end
@@ -32,4 +39,9 @@ module sqr_gen (en,
     .address(addr[18:9]),
     .data(data)
     );
+    
+    sqr_table sqr_phase(
+    .address(addr[18:9] + phase),
+    .data(data_phase)
+    )
 endmodule
